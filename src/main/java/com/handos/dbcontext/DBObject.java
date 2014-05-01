e8fe3ffb82e5e47ae5f45c2affab436250b1450f
jimmyleeking <l02n20l@qq.com>
2014-05-01 21:07:01 +0800

    修改DBObject定义

--- src/main/java/com/handos/dbcontext/DBObject.java
+++ src/main/java/com/handos/dbcontext/DBObject.java
@@ -1,8 +1,37 @@
 package com.handos.dbcontext;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jimmylee on 5/1/14.
  */
 public class DBObject {
 
+    /**
+     * 数据表名
+     */
+    private String tableName;
+
+    public String getTableName() {
+        return tableName;
+    }
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    /**
+     * 键值
+     */
+    private Map<String,Object> map=new HashMap<String, Object>();
+
+    /**
+     *
+     * @param key
+     * @param value
+     */
+    public void put(String key,Object value)
+    {
+        this.map.put(key,value);
+    }
 }
