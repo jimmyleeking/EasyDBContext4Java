f6140b493c6849611bac3f56d23d3692da042b1c
jimmyleeking <l02n20l@qq.com>
2014-05-02 12:21:21 +0800

    修改文件名

--- src/main/java/com/handos/easycloudlog/EasyCloudLog.java
+++ src/main/java/com/handos/easycloudlog/EasyCloudLog.java
@@ -5,9 +5,9 @@ import com.handos.dbcontext.DBContextFactory;
 /**
  * Created by jimmylee on 5/2/14.
  */
-public class CloudLog {
+public class EasyCloudLog {
 
-    private CloudLog(){
+    private EasyCloudLog(){
 
     }
     private static ILog log =new LogImpl(DBContextFactory.getDBContext());
@@ -17,12 +17,12 @@ public class CloudLog {
     }
 
     public static void debug(String tag, String content, Throwable tr) {
-        log.debug(tag,content,tr);
+        log.debug(tag, content, tr);
     }
 
 
     public static void error(String tag, String content) {
-        log.error(tag,content);
+        log.error(tag, content);
 
     }
 
@@ -31,32 +31,32 @@ public class CloudLog {
     }
 
     public static void info(String tag, String content) {
-        log.info(tag,content);
+        log.info(tag, content);
     }
 
 
     public static void info(String tag, String content, Throwable tr) {
-        log.info(tag,content,tr);
+        log.info(tag, content, tr);
     }
 
 
     public static void verbose(String tag, String content) {
-        log.verbose(tag,content);
+        log.verbose(tag, content);
     }
 
 
     public static void verbose(String tag, String content, Throwable tr) {
-        log.verbose(tag,content,tr);
+        log.verbose(tag, content, tr);
     }
 
 
     public static void warn(String tag, String content) {
-        log.warn(tag,content);
+        log.warn(tag, content);
     }
 
 
     public static void warn(String tag, String content, Throwable tr) {
-        log.warn(tag,content,tr);
+        log.warn(tag, content, tr);
     }
 
 
